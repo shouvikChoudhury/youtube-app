0bfc79ea146819d9213e2ddded1d9de13f33151c
Shouvik Choudhury <98959824+shouvikChoudhury@users.noreply.github.com>
2023-03-08 19:08:56 +0530

    Add files via upload

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -26,7 +26,7 @@ const LeftNav = () => {
 
   return (
     <div
-      className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${mobileMenu ? "translate-x-0" : ""
+      className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 md:translate-x-0 transition-all ${mobileMenu ? "" : "translate-x-[-240px]"
         }`}
     >
       <div className="flex px-5 flex-col">
@@ -41,8 +41,8 @@ const LeftNav = () => {
                   navigate("/");
                 }}
                 className={`${selectedCategory === item.name
-                    ? "bg-white/[0.15]"
-                    : ""
+                  ? "bg-white/[0.15]"
+                  : ""
                   }`}
               />
               {item.divider && (
